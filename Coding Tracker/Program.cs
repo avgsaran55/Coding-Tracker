bb4cb7f927cdf8a9e13dfd41bd689ead6d39f804
avgsaran55 <sarankumar282002@gmail.com>
2026-04-02 15:27:26 +0530

    Added three method

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-getMethod();
-void getMethod()
+firstMethod();
+void firstMethod()
+{
+    Console.WriteLine("Hello, World! saran one more modification");
+    Console.WriteLine("Hello, World! saran one more modification for fetch keywords");
+}
+void fsecondMethod()
+{
+    Console.WriteLine("Hello, World! saran one more modification");
+    Console.WriteLine("Hello, World! saran one more modification for fetch keywords");
+}
+void ThirdMethod()
 {
     Console.WriteLine("Hello, World! saran one more modification");
     Console.WriteLine("Hello, World! saran one more modification for fetch keywords");
